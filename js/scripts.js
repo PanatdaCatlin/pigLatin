9fc08e630ec3a7b967a07c570052c5ff2f723075
Panatda Inthala and tyler kostelak <tyler@>
2018-01-18 16:51:36 -0800

    javascript sorts words by vowels, if first letter is vowel it adds way.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,89 @@
-// business logic
-
-
-
-
-//user interface logic
 $(document).ready(function() {
+
+  var vowels = ["a", "e", "i", "o", "u"];
+  var userInput;
+  var firstLetterVowel = false;
+  var results;
+  var setFirstLetterVowel = function(userInput) {
+
+    var firstLetter = userInput[0];
+    vowels.forEach(function(vowel) {
+      if (firstLetter === vowel) {
+        firstLetterVowel = true;
+      };
+    });
+  };
+
+
+  $("#pig-latin").submit(function(event) {
+    userInput = $("#word-input").val();
+    userInput = userInput.split("");
+
+    setFirstLetterVowel(userInput);
+    console.log(firstLetterVowel);
+
+    if (firstLetterVowel === true) {
+      userInput = userInput.join("");
+        userInput = userInput.concat("-way");
+          $("#result").text(userInput);
+            firstLetterVowel = false;
+
+    };
+    alert (userInput);
+    if (firstLetterVowel === false) {};
+
+
+
+
+      alert(typeof(userInput))
+    event.preventDefault();
+
+  });
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//   alert (findVowel);
+//
+//
+//
+//
+// //user interface logic
+// $(document).ready(function() {
+//   $("#pig-latin").submit(function(event) {
+//     event.preventDefault();
+//     var userInput = $("input#word-input").val();
+//     var resultShow = wordInput(userInput);
+//     $("#result").show();
+//     $("#result").text(resultShow);
+//
+//
+//   });
+// });
